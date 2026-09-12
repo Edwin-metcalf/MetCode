297d5aa3d5cbffc36caf724b19f43952ec33b24e
Edwin Metcalf <winmetcalf1@gmail.com>
2026-09-11 17:42:29 -0700

    better testind and a readme

---
+++ README.md
@@ -0,0 +1,22 @@
+<div align="center">
+  ```
+ __  __      _   ____          _      
+|  \/  | ___| |_/ ___|___   __| | ___ 
+| |\/| |/ _ \ __| |  / _ \ / _` |/ _ \
+| |  | |  __/ |_| |__| (_) | (_| |  __/
+|_|  |_|\___|\__\____\___/ \__,_|\___|
+  ```
+</div>
+
+Ran out of Claude Code tokens? Tokens too expensive? Don't want to send all your data to some big company? I wanted to better understand how agentic coding assistants work?
+Welcome to MetCode! A command line chat client for local LLMS. MetCode is built in Go on top of Ollama. 
+
+---
+## MetCode includes
+* **Chatting with Local Ollama Server** Converse with context and all local, no cost, no limits, and no security issues!
+* **Model Selection** Download and use whatever model you want
+* **File Reading** ability to read files on your system
+* **File Creation and Editing** Can create new files or edit pre existing files 
+
+## More stuff coming!
+
--- main_test.go
+++ main_test.go
@@ -13,7 +13,7 @@ func TestReadDirectoryHelper_ValidFile(t *testing.T) {
 		},
 	}
 
-	result := readDirectoryHelper(call)
+	result := readFileHelper(call)
 
 	if result.Role != "tool" {
 		t.Errorf("Role is not tool it is %v", result.Role)
@@ -30,8 +30,40 @@ func TestReadDirectoryHelper_NoPath(t *testing.T) {
 			Arguments: map[string]any{},
 		},
 	}
-	result := readDirectoryHelper(call)
+	result := readFileHelper(call)
 	if !strings.Contains(result.Content, "error") {
 		t.Errorf("expected error but got %q", result.Content)
+	}
+}
+
+func TestProtectedFiles(t *testing.T) {
+	call := &toolCall{
+		Function: calledFunction{
+			Name:      "edit_file",
+			Arguments: map[string]any{"path": "main.go", "old_text": "package main\ngood code:)", "new_text": "bad code:("},
+		},
+	}
+	result := editFileHelper(call)
+
+	if !strings.Contains(result.Content, "error") {
+		t.Errorf("expected error but got %v", result.Content)
+	}
+}
+
+func TestEditFileValid(t *testing.T) {
+	call := &toolCall{
+		Function: calledFunction{
+			Name:      "edit_file",
+			Arguments: map[string]any{"path": "example.go", "old_text": "bad stuff :(", "new_text": "good stuff :)"},
+		},
+	}
+	result := editFileHelper(call)
+
+	if result.Role != "tool" {
+		t.Errorf("error: Role is not a tool it is: %v", result.Role)
+	}
+
+	if result.Content == "" {
+		t.Error("content is empty")
 	}
 }
